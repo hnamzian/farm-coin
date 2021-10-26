dc826f42710d08a554527acb043b064024427273
hnamzian <namazian87@gmail.com>
2021-10-26 18:23:51 +0300

    Add getters to stake states

--- contracts/stake/StakeStates.sol
+++ contracts/stake/StakeStates.sol
@@ -50,4 +50,73 @@ contract StakeStates is LockupPeriod {
      * @dev total amount of stakes held by all address at each LockupOption
      */
     mapping(LockupOption => uint256) internal _totalStakes;
+
+
+    /**
+     * @dev returns total staked amount of tokens to a specified lockup optin by all addresses
+     * @param lockupOption_ reward plan which can be: (NO_LOCKUP, SIX_MONTH_LOCKUP, ONE_YEAR_LOCKUP)
+     * @return uint256 total amount of tokens staked to lockupOption_
+     */
+    function totalStakesLockup(LockupOption lockupOption_)
+        public
+        view
+        returns (uint256)
+    {
+        return _totalStakes[lockupOption_];
+    }
+
+    /**
+     * @dev returns total staked amount of tokens over all lockup optins by all addresses
+     * @return _totalStaked uint256 total amount of tokens staked over all lockup optins by all addresses
+     */
+    function totalStakes()
+        public
+        view
+        returns (uint256 _totalStaked)
+    {
+        uint8 _lockupOption;
+        for (
+            _lockupOption = uint8(LockupOption.NO_LOCKUP);
+            _lockupOption <= uint8(LockupOption.ONE_YEAR_LOCKUP);
+            _lockupOption++
+        ) {
+            _totalStaked += totalStakesLockup(LockupOption(_lockupOption));
+        }
+    }
+
+    /**
+     * @dev returns total staked amount of tokens to a specified lockup optin by a specified address
+     * @param lockupOption_ reward plan which can be: (NO_LOCKUP, SIX_MONTH_LOCKUP, ONE_YEAR_LOCKUP)
+     * @param staker_ address of staker
+     * @return uint256 total amount of tokens staked to a specified lockup optin by the staker
+     */
+    function stakesLockupOf(
+        LockupOption lockupOption_,
+        address staker_
+    ) public view returns (uint256) {
+        return _stakes[lockupOption_][staker_];
+    }
+
+    /**
+     * @dev returns total staked amount of tokens over all lockup optins by a specified address
+     * @param staker_ address of staker
+     * @return _totalStakesOf uint256 total amount of tokens staked over all lockup optins by the staker
+     */
+    function stakesOf(address staker_)
+        public
+        view
+        returns (uint256 _totalStakesOf)
+    {
+        uint8 _lockupOption;
+        for (
+            _lockupOption = uint8(LockupOption.NO_LOCKUP);
+            _lockupOption <= uint8(LockupOption.ONE_YEAR_LOCKUP);
+            _lockupOption++
+        ) {
+            _totalStakesOf += stakesLockupOf(
+                LockupOption(_lockupOption),
+                staker_
+            );
+        }
+    }
 }
