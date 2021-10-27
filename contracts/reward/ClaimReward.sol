a7638a0c621deb1e0d6798ac7b0fe2ed6a209ca3
hnamzian <namazian87@gmail.com>
2021-10-27 22:33:19 +0300

    Implement rewarding tokens to staker

---
+++ contracts/reward/ClaimReward.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./RewardCalculator.sol";
+
+contract ClaimReward is RewardCalculator {
+
+    /**
+     * @dev assign amount of tokens must be rewarded to a specified rewardee
+     * according to its stakes at a specified lockup option
+     * @param lockupOption_ uint8 representing Reward lockup option
+     * @param rewardee_ account address of rewardee 
+     */
+    function _reward(LockupOption lockupOption_, address rewardee_) internal {
+        uint256 _rewardsOf = _calculateRewardsOf(lockupOption_, rewardee_);
+
+        _increaseTotalRewardsOf(lockupOption_, rewardee_, _rewardsOf);
+        _increaseTotalRewards(lockupOption_, _rewardsOf);
+        _updateLastTimeRewardedTo(lockupOption_, rewardee_);
+    }
+
+    /**
+     * @dev assign amount of tokens must be rewarded to reward claimee
+     * according to its stakes at a specified lockup option
+     * @param lockupOption_ uint8 representing Reward lockup option
+     */
+    function claim(LockupOption lockupOption_) public {
+        _reward(lockupOption_, msg.sender);
+    }
+
+    /**
+     * @dev assign amount of tokens must be rewarded to reward claimee
+     * according to its stakes at all lockup options
+     */
+    function claim() public {
+        uint8 _lockupOption;
+        for (
+            _lockupOption = uint8(LockupOption.NO_LOCKUP);
+            _lockupOption <= uint8(LockupOption.ONE_YEAR_LOCKUP);
+            _lockupOption++
+        ) {
+            claim(LockupOption(_lockupOption));
+        }
+    }
+}
