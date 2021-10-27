f26dca54f063c188536b9675b1f37e75bfeaa3db
hnamzian <namazian87@gmail.com>
2021-10-27 22:51:32 +0300

    Override _reward function to mint FarmCoin tokens while rewarding

--- contracts/FarmCoin.sol
+++ contracts/FarmCoin.sol
@@ -10,4 +10,15 @@ contract FarmCoin is ERC20, ClaimReward, BaseStake {
     constructor(string memory name_, string memory symbol_)
         ERC20(name_, symbol_)
     {}
+
+    function _reward(LockupOption lockupOption_, address rewardee_)
+        internal
+        virtual
+        override
+        returns (uint256 _rewardsOf)
+    {
+        _rewardsOf = super._reward(lockupOption_, rewardee_);
+
+        _mint(rewardee_, _rewardsOf);
+    }
 }
