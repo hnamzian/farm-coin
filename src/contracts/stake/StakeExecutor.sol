a3b714356a9d94654e74aa401e96fd16ce620fb0
hnamzian <namazian87@gmail.com>
2021-10-28 08:12:09 +0300

    Set USDC address as stakingToken

--- src/contracts/stake/StakeExecutor.sol
+++ src/contracts/stake/StakeExecutor.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract StakeExecutor {
     using SafeERC20 for IERC20;
 
-    address internal stakingToken;
+    address internal stakingToken = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     /**
      * @dev transferFrom Staking Token (USDC) from staker to Stake contract if allowed
