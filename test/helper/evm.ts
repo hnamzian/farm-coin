aa74ad195e4bbc9d3d5861bae8e6653f24f84f66
hnamzian <namazian87@gmail.com>
2021-10-28 16:04:48 +0300

    Implement getTimestamp to get evm timestamp

--- test/helper/evm.ts
+++ test/helper/evm.ts
@@ -9,4 +9,11 @@ export class EVM {
   static increaseEVMTimestamp = async (seconds: number) => {
     await ethers.provider.send("evm_increaseTime", [seconds]);
   };
+
+  static getTimestamp = async () => {
+    const blockNumber = await ethers.provider.getBlockNumber();
+    const { timestamp } = await ethers.provider.getBlock(blockNumber);
+    return parseInt(timestamp.toString());
+  };
+  
 }
